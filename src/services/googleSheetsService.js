f78b5fcdc0e6858476c4f03cdc92d873dd60f054
YuliethVega27 <a01641780@tec.mx>
2024-12-24 00:13:42 -0600

    Se modificó sheets credentials

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import { google } from 'googleapis';
 
 const sheets = google.sheets('v4');
@@ -6,7 +5,7 @@ const sheets = google.sheets('v4');
 async function addRowToSheet(auth, spreadsheetId, values) {
     const request = {
         spreadsheetId,
-        range: 'reservas', //hoja
+        range: 'reservas', // Nombre de la hoja
         valueInputOption: 'RAW',
         insertDataOption: 'INSERT_ROWS',
         resource: {
@@ -19,23 +18,35 @@ async function addRowToSheet(auth, spreadsheetId, values) {
         const response = await sheets.spreadsheets.values.append(request);
         return response;
     } catch (error) {
-        console.error(error);
+        console.error('Error al añadir datos a la hoja:', error);
     }
 }
 
-const appendToSheet = async(data) => {
+const appendToSheet = async (data) => {
     try {
+        // Configuración del cliente de autenticación utilizando variables de entorno
         const auth = new google.auth.GoogleAuth({
-            keyFile: path.join(process.cwd(), 'src/credentials', 'credentials.json'),
+            credentials: {
+                type: process.env.TYPE,
+                project_id: process.env.PROJECT_ID,
+                private_key_id: process.env.PRIVATE_KEY_ID,
+                private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Asegúrate de que las nuevas líneas estén correctamente formateadas
+                client_email: process.env.CLIENT_EMAIL,
+                client_id: process.env.CLIENT_ID,
+                auth_uri: process.env.AUTH_URI,
+                token_uri: process.env.TOKEN_URI,
+                auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+                client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+            },
             scopes: ['https://www.googleapis.com/auth/spreadsheets']
         });
 
-        const authClient = await auth.getClient(); // Corrección aquí para obtener el cliente autenticado
-        const spreadsheetId = '1dDDArNS2U22QthZHMJa2IiPX53pTkjUIPPuvGQ8fp7U';
+        const authClient = await auth.getClient(); // Obtener el cliente autenticado
+        const spreadsheetId = '1dDDArNS2U22QthZHMJa2IiPX53pTkjUIPPuvGQ8fp7U'; // ID de tu hoja de cálculo
         await addRowToSheet(authClient, spreadsheetId, data);
         return 'Datos correctamente agregados';
     } catch (error) {
-        console.error(error);
+        console.error('Error al acceder a Google Sheets:', error);
     }
 }
 
